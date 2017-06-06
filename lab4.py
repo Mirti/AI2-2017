a326ef89948c3b0cc4547ec8d3b2529d7dda8225
Mirti <avalith@o2.pl>
2017-06-06 22:59:59 +0200

    Add missing methods to lab4

--- lab4.py
+++ lab4.py
@@ -1,5 +1,6 @@
 import operator
 import functools
+import itertools
 
 def test_lambda():
   print((lambda val: val ** 2)(5)) 
@@ -71,10 +72,10 @@ def float_Control(score):
         return "Tied"
 
 def replace_Return(*args):
-  echo = lambda arg: arg  # In practice, you should never bind lambdas to local names
-  cond_fn = lambda x: (x==1 and echo("one")) \
-                 or (x==2 and echo("two")) \
-                 or (echo("other"))
+  echo = lambda arg: arg  
+  cond_fn = lambda x: (x==1 and print("one")) \
+                 or (x==2 and print("two")) \
+                 or (print("other"))
 
 def replace_Loop(lst,func):
   map(func,lst)
@@ -86,4 +87,140 @@ def iterator_Consumption():
   print(next(it))  # => 68
   (37 in it)  # => Error
   (next(it))  # => StopIteration
-  
+
+def test_itertools():
+    for el in itertools.permutations('XKCD', 2):
+        print(el, end=', ')
+
+    for el in itertools.cycle('LO'):
+        print(el, end='')  # Don't run this one. Why not?
+
+    itertools.starmap(operator.mul, itertools.zip_longest([3,5,7],[2,3], fillvalue=1))
+
+def dot_product(u, v):
+    assert len(u) == len(v)
+    return sum(itertools.starmap(operator.mul, zip(u, v)))
+
+def transpose(m):
+    return tuple(zip(*m))
+
+def test_transpose():
+    matrix = (
+    (1, 2, 3, 4),
+    (5, 6, 7, 8),
+    (9,10,11,12)
+    )
+    print(transpose(matrix))
+
+def matmul(m1, m2):
+    return tuple(map(lambda row: tuple(dot_product(row, col) for col in transpose(m2)), m1))
+
+def generate_triangles():
+    n = 0
+    total = 0
+    while True:
+        total += n
+        n += 1
+        yield total
+
+
+def primes_under(n):
+    tests = []
+    for i in range(2, n):
+        if not any(map(lambda test: test(i), tests)):
+            tests.append(make_divisibility_test(i))
+            yield i
+
+"""
+def outer():
+    def inner(a):
+        return a
+    return inner
+
+f = outer()
+print(f)  # <function outer.<locals>.inner at 0x1044b61e0>
+f(10)  # => 10
+
+f2 = outer()
+print(f2)  # <function outer.<locals>.inner at 0x1044b6268> (Different from above!)
+f2(11)  # => 11
+"""
+
+def make_adder(n):
+    def add_n(m):  # Captures the outer variable `n` in a closure
+        return m + n
+    return add_n
+
+def test_make_adder():
+    add1 = make_adder(1)
+    print(add1)  # <function make_adder.<locals>.add_n at 0x103edf8c8>
+    add1(4)  # => 4
+    add1(5)  # => 6
+    add2 = make_adder(2)
+    print(add2)  # <function make_adder.<locals>.add_n at 0x103ecbf28>
+    add2(4)  # => 6
+    add2(5)  # => 7
+    
+    closure = add1.__closure__
+    cell0 = closure[0]
+    cell0.cell_contents  # => 1 (this is the n = 1 passed into make_adder)
+
+def foo(a, b, c=-1, *d, e=-2, f=-3, **g):
+    def wraps():
+        print(a, c, e, g)
+
+def test_foo():
+    w = foo(1, 2, 3, 4, 5, e=6, f=7, y=2, z=3)
+    print(list(map(lambda cell: cell.cell_contents, w.__closure__)))
+
+def outer(l):
+    def inner(n):
+        return l * n
+    return inner
+
+def test_outer():
+    l = [1, 2, 3]
+    f = outer(l)
+    print(f(3))
+    l.append(4)
+    print(f(3))
+
+def print_args(function):
+    def wrapper(*args, **kwargs):
+        # (1) You could do something here
+	    retval = function(*args, **kwargs)
+		# (2) You could also do something here
+	    return retval
+    return wrapper
+
+@cache
+def fib(n):
+    return fib(n-1) + fib(n-2) if n > 2 else 1
+
+def test_fib():
+    fib(10)  # 55 (takes a moment to execute)
+    fib(10)  # 55 (returns immediately)
+    fib(100) # doesn't take forever
+    fib(400) # doesn't raise RuntimeError
+
+@enforce_types
+def foo2(a: int, b: str) -> bool:
+    if a == -1:
+        return 'Gotcha!'
+    return b[a] == 'X'
+
+def test_foo2():
+    foo(3, 'abcXde')  # => True
+    foo(2, 'python')  # => False
+    foo(1, 4)  # prints "Invalid argument type for b: expected str, received int
+    foo(-1, '')  # prints "Invalid return type: expected bool, received str
+
+@enforce_types(severity=2)
+def bar(a: list, b: str) -> int:
+    return 0
+
+@enforce_types()  # Note that there are parentheses
+def baz(a: bool, b: str) -> str:
+    return ''
+    
+
